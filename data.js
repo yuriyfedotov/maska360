b240113fea11b3725d609feb9dc7b3bca5122462
Y_Fedotov <yurz.vf@gmail.com>
2023-09-08 22:21:46 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,758 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1--",
+      "name": "1 Главный вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.10425265631323555,
+        "pitch": 0.12383367937073153,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.06286698554217196,
+          "pitch": 0.021319141173416156,
+          "rotation": 0,
+          "target": "0-1--"
+        },
+        {
+          "yaw": -0.18052002938562595,
+          "pitch": 0.056833077394049525,
+          "rotation": 0,
+          "target": "1-2--"
+        },
+        {
+          "yaw": 1.3820310034378362,
+          "pitch": 0.11064126263307017,
+          "rotation": 0,
+          "target": "12-12----"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.34490415369652894,
+          "pitch": -0.43565108768520666,
+          "title": "Главный вход",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-2--",
+      "name": "2 Внутренний двор",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.0067128048153630715,
+        "pitch": 0.19589288874075272,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2657709671173993,
+          "pitch": 0.06888032255419141,
+          "rotation": 0,
+          "target": "4-5--"
+        },
+        {
+          "yaw": 0.0725666892018797,
+          "pitch": 0.11219290490438638,
+          "rotation": 0,
+          "target": "2-3-"
+        },
+        {
+          "yaw": 0.8684567039280129,
+          "pitch": 0.04651124768110826,
+          "rotation": 0,
+          "target": "6-7-"
+        },
+        {
+          "yaw": 0.25310962603589005,
+          "pitch": 0.025976677713089202,
+          "rotation": 0,
+          "target": "5-6-"
+        },
+        {
+          "yaw": 0.6413786494322125,
+          "pitch": 0.04316131310778282,
+          "rotation": 0,
+          "target": "8-9-"
+        },
+        {
+          "yaw": 0.4530340663035375,
+          "pitch": 0.041277036120797916,
+          "rotation": 0,
+          "target": "10-11-"
+        },
+        {
+          "yaw": -2.758758666997643,
+          "pitch": 0.049300194329124025,
+          "rotation": 0,
+          "target": "9-10--"
+        },
+        {
+          "yaw": -1.975031805978027,
+          "pitch": 0.036188640429415386,
+          "rotation": 0,
+          "target": "11-11--"
+        },
+        {
+          "yaw": -2.4567628724581994,
+          "pitch": 0.071020699058435,
+          "rotation": 0,
+          "target": "13-13--"
+        },
+        {
+          "yaw": -0.6257229091860754,
+          "pitch": -0.011515909376413447,
+          "rotation": 0,
+          "target": "3-4--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.20333356911069345,
+          "pitch": -0.3603076041382707,
+          "title": "Внутренний двор",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "2-3-",
+      "name": "3 Веранда",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.09961159854079504,
+        "pitch": 0.3262822894482351,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9902727924899395,
+          "pitch": 0.07472636860944526,
+          "rotation": 0,
+          "target": "3-4--"
+        },
+        {
+          "yaw": 1.313457827756384,
+          "pitch": 0.001214959114546943,
+          "rotation": 0,
+          "target": "1-2--"
+        },
+        {
+          "yaw": -0.6271425476988632,
+          "pitch": 0.057573756733084736,
+          "rotation": 0,
+          "target": "5-6-"
+        },
+        {
+          "yaw": -0.20499149282956708,
+          "pitch": 0.06680234959794085,
+          "rotation": 0,
+          "target": "10-11-"
+        },
+        {
+          "yaw": 0.3573121626990954,
+          "pitch": 0.05101358644765597,
+          "rotation": 0,
+          "target": "8-9-"
+        },
+        {
+          "yaw": 0.7928838981913344,
+          "pitch": 0.021776879897068113,
+          "rotation": 0,
+          "target": "6-7-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.347313327179279,
+          "pitch": -0.1801026345639798,
+          "title": "Веранда",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "3-4--",
+      "name": "4 Основной зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.15192626984955027,
+        "pitch": 0.26603068853506784,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9845738816593794,
+          "pitch": 0.20484716150800608,
+          "rotation": 0,
+          "target": "2-3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.16251586921709738,
+          "pitch": -0.29238489143867596,
+          "title": "Основной зал",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "4-5--",
+      "name": "5 Банкетный зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.592257208529885,
+        "pitch": 0.24417515986631955,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.945566055933689,
+          "pitch": 0.19955424876532213,
+          "rotation": 0,
+          "target": "1-2--"
+        },
+        {
+          "yaw": -1.3414234274444823,
+          "pitch": 0.09334019409662986,
+          "rotation": 0,
+          "target": "12-12----"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-6-",
+      "name": "6 Беседка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.2649498763670941,
+        "pitch": 0.3641875786202,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.2180385446323845,
+          "pitch": 0.25344330146003813,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-7-",
+      "name": "7 Беседка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.609420270948009,
+        "pitch": 0.32997852496646374,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7525658259705983,
+          "pitch": 0.2508823566019238,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-8-",
+      "name": "8 Беседка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.6095738827855417,
+        "pitch": 0.45784048612973294,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7609835820543491,
+          "pitch": 0.20115861598942963,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-9-",
+      "name": "9 Беседка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.3861002293859972,
+        "pitch": 0.4678440579867864,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8964374350972442,
+          "pitch": 0.32391524596663857,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.42961950818322414,
+          "pitch": 0.22731168465196383,
+          "title": "Беседка",
+          "text": "Беседка"
+        }
+      ]
+    },
+    {
+      "id": "9-10--",
+      "name": "10 Охотничий домик",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.21320458320615465,
+        "pitch": 0.25021462041432585,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6486282609231777,
+          "pitch": 0.16254273524336327,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11-",
+      "name": "11 Беседка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.529464880972288,
+        "pitch": 0.2827535589573209,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9044085835363447,
+          "pitch": 0.23331083683789977,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-11--",
+      "name": "11 Вип домик",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.004027682889217843,
+        "pitch": 0.2993861212489275,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3425084530504137,
+          "pitch": 0.22149004186198695,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.004027682889217843,
+          "pitch": 0.2993861212489275,
+          "title": "Вип домик",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "12-12----",
+      "name": "12 Вход в банкетный зал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.3047662370533839,
+        "pitch": -0.04250543696744025,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.38681300203101543,
+          "pitch": -0.016860785764238884,
+          "rotation": 0,
+          "target": "4-5--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.3047662370533839,
+          "pitch": -0.04250543696744025,
+          "title": "Вход в банкетный зал",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "13-13--",
+      "name": "13 Детская площадка",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.0579380389011845,
+        "pitch": 0.14414573624036287,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19017905189442352,
+          "pitch": 0.06016194407443365,
+          "rotation": 0,
+          "target": "11-11--"
+        },
+        {
+          "yaw": 3.094374275636234,
+          "pitch": 0.12676001579148277,
+          "rotation": 0,
+          "target": "9-10--"
+        },
+        {
+          "yaw": 1.2131500252363487,
+          "pitch": 0.14029773770099396,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0579380389011845,
+          "pitch": 0.14414573624036287,
+          "title": "Детская площадка",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "14-14-",
+      "name": "14 Вход",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.051731386729312,
+        "pitch": 0.0918572262689814,
+        "fov": 1.395287170018175
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.051731386729312,
+          "pitch": 0.0918572262689814,
+          "rotation": 0,
+          "target": "1-2--"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 3.051731386729312,
+          "pitch": 0.0918572262689814,
+          "title": "Вход",
+          "text": "Вход"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
